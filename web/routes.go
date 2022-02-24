f51fd4e1c6c700f783bb2c868b1bdd9c9db1d668
Resoluter <xujabekovsirojiddin@gmail.com>
2022-02-24 16:46:33 +0500

    Initial commit

---
+++ web/routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/resoluter/booking/pkg/config"
+	"github.com/resoluter/booking/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	// mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
