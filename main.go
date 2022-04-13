0f53fd818e573e71429ac388c779bd1cb6195fcc
Sirojiddin Khujabekov <xujabekovsirojiddin@gmai.com>
2022-04-13 22:59:30 +0500

    commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/resoluter/booking/internal/config"
 	"github.com/resoluter/booking/internal/handlers"
+	"github.com/resoluter/booking/internal/models"
 	"github.com/resoluter/booking/internal/render"
 )
 
@@ -18,8 +20,10 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	gob.Register(models.Reservation{})
 
 	app.InProduction = false
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
