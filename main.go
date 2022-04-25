98a0a7e940e1e850d9d37ad5b413eafec1fef334
Sirojiddin Khujabekov <xujabekovsirojiddin@gmai.com>
2022-04-25 23:09:57 +0500

    error handling

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/resoluter/booking/internal/config"
 	"github.com/resoluter/booking/internal/handlers"
+	"github.com/resoluter/booking/internal/helpers"
 	"github.com/resoluter/booking/internal/models"
 	"github.com/resoluter/booking/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -28,7 +32,7 @@ func main() {
 	// http.HandleFunc("/", Repo.Home)
 	// http.HandleFunc("/about", Repo.About)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Printf("Starting application on port %s\n", portNumber)
 	// _ = http.ListenAndServe(portNumber, nil)
 
 	srv := &http.Server{
@@ -44,6 +48,12 @@ func run() error {
 	gob.Register(models.Reservation{})
 
 	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -64,8 +74,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
